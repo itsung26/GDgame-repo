2459e065928aa5880ee832741db76095a4728e17
itsung26 <iat2016@icloud.com>
2025-08-18 18:20:18 -0400

    added

---
+++ scripts/character_body_3d.gd
@@ -0,0 +1,52 @@
+extends CharacterBody3D
+
+@onready var player: CharacterBody3D = $"."
+@onready var camera_3d: Camera3D = %Camera3D
+@onready var pivot: Node3D = $Pivot
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+@export var SPEED = 5.0
+@export var JUMP_VELOCITY = 4.5
+@export var look_sensitivity = 0.1
+
+func _ready() -> void:
+	# set the mouse to be captured by the gamewindow
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _input(event) -> void:
+	# handle mouselook
+	if event is InputEventMouseMotion:
+		var mouse_delta = event.relative
+		var yaw = -mouse_delta.x
+		var pitch = -mouse_delta.y
+		player.rotate_y(deg_to_rad(look_sensitivity * yaw))
+		pivot.rotate_x(deg_to_rad(look_sensitivity * pitch))
+
+func _physics_process(delta: float) -> void:
+	if velocity != Vector3.ZERO:
+		animation_player.play("weaponBob")
+	else:
+		animation_player.stop()
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "forward", "back")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
---
+++ scripts/game_manager.gd
@@ -0,0 +1,22 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+var ispaused = false
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta) -> void:
+	if Input.is_action_just_pressed("forcequit"):
+		get_tree().quit()
+	
+	if Input.is_action_just_pressed("pause"):
+		if not ispaused:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			get_tree().paused = true
+			ispaused = true
+		elif ispaused:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			get_tree().paused = false
+			ispaused = false
