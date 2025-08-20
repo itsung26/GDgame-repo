a3b9024bacb08214f2cea2c5cd29823f699cbbec
itsung26 <iat2016@icloud.com>
2025-08-20 10:40:18 -0400

    separated bullet ray cast into a class

--- scripts/blaster.gd
+++ scripts/blaster.gd
@@ -1,9 +1,10 @@
 extends Skeleton3D
 
 @onready var animation_player: AnimationPlayer = $"../../../AnimationPlayer"
-@onready var ray_cast_3d: RayCast3D = $"../RayCast3D"
+@onready var bullet_ray_cast: RayCast3D = $"../BulletRayCast"
 
-const BULLET_DECAL = preload("res://scenes/bullet_decal.tscn")
+
+const BULLET_DECAL_BLUE = preload("res://scenes/bullet_decal.tscn")
 
 signal current_blaster_ammo(amount)
 signal current_anim(anim)
@@ -18,8 +19,8 @@ const DAMAGE = 5
 # this method is called every time the animation for firing runs and is essentially the bulk of the shoot code
 # refer to the _process method for the animations, which are triggered by only input
 func fire():
-	var body = ray_cast_3d.get_collider()
-	var b = BULLET_DECAL.instantiate()
+	var body = bullet_ray_cast.get_collider()
+	var b = BULLET_DECAL_BLUE.instantiate()
 	var b_mesh = b.get_node("MeshInstance3D")
 	var b_surface_override = b_mesh.get_surface_override_material(0)
 	
@@ -35,8 +36,8 @@ func fire():
 	# in other words, IF IT HITS SOMETHING DO THIS VVVVVV
 	if body != null:
 		body.add_child(b)
-		b.global_transform.origin = ray_cast_3d.get_collision_point()
-		b.look_at(ray_cast_3d.get_collision_point() + ray_cast_3d.get_collision_normal(), Vector3.UP)
+		b.global_transform.origin = bullet_ray_cast.get_collision_point()
+		b.look_at(bullet_ray_cast.get_collision_point() + bullet_ray_cast.get_collision_normal(), Vector3.UP)
 		if body.is_in_group("enemy"):
 			body.health = body.health - DAMAGE
 			print("health of enemy: " + str(body.health))
@@ -80,3 +81,7 @@ func _process(_delta) -> void:
 	if Input.is_action_pressed("reload"):
 		if ammo != 50:
 			animation_player.play("reload_pistol")
+			
+	# special action
+	if Input.is_action_just_pressed("right click action"):
+		print("action")
