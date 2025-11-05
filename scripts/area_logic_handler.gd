44b8b9a18429de131408441dd41c4b03c2cf85f7
itsung26 <iat2016@icloud.com>
2025-11-05 01:15:38 -0500

    modeled more assets for the winds level

---
+++ scripts/area_logic_handler.gd
@@ -0,0 +1,11 @@
+## Handles area3d logic
+extends Node
+
+@onready var tablet_text: MeshInstance3D = $"../the cold winds map/cold_winds/startingZone/Sci-fi Tablet/tabletText"
+
+func _on_tablet_interact_body_entered(body: Player) -> void:
+	tablet_text.visible = true
+
+
+func _on_tablet_interact_body_exited(body: Player) -> void:
+	tablet_text.visible = false
