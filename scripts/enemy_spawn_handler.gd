8816d2ad4bcb09d298dba88c0f71efd2dd0a2d2c
itsung26 <iat2016@icloud.com>
2026-03-10 14:47:56 -0400

    balance

--- scripts/enemy_spawn_handler.gd
+++ scripts/enemy_spawn_handler.gd
@@ -1,3 +1,6 @@
+## Spawns instances of a single enemy scene at the positions of child Marker3D nodes.
+## Supports optional delays between each individual spawn, measured in seconds and
+## independent of framerate.
 class_name EnemySpawnHandler
 extends Node
 
@@ -12,15 +15,18 @@ extends Node
 ## 
 ## This object is instanceable mid-runtime and [code]spawnEnemies[/code] can be called several times.
 
-## If true, calls spawnEnemies() at time of being loaded.
+## If true, calls spawnEnemies() when this node enters the scene tree.
 @export var spawn_enemies_on_ready:bool = false
-## This is the scene that is instantiated. It does not strictly have to be an enemy, but it is strongly reccomended to use EnemySpawnHandler for exclusively enemies.
+## Scene to instantiate for each spawn point. Intended to be an Enemy-derived scene.
 @export var enemy_to_spawn_SCENE:PackedScene
+## Optional delay (in seconds) between spawning each individual enemy. 0.0 spawns all at once.
+@export var spawn_delay_between_enemies:float = 0.1
 
-## The children of this node. They should only be a marker3d.
+## Cached list of child spawn points. These should only be Marker3D nodes.
 @onready var spawn_points:Array = get_children()
 
 
+## Validates configuration and optionally spawns enemies immediately if enabled.
 func _ready() -> void:
 	assert(enemy_to_spawn_SCENE != null)
 	
@@ -36,14 +42,21 @@ func _ready() -> void:
 	else:
 		print("ERROR: No children detected")
 
-## Instances the scene and spawns an instance at each child marker3d node. Retuns an array of the enemies spawned.
+## Instances the configured scene and spawns an instance at each child Marker3D spawn point.
+## Returns an array of the spawned enemies. If [member spawn_delay_between_enemies] is > 0.0,
+## this function asynchronously waits in seconds between each spawn in a framerate-independent way.
 func spawnEnemies() -> Array[Enemy]:
 	var ret:Array[Enemy] = []
-	for spawn_point:Marker3D in spawn_points:
+	for i in spawn_points.size():
+		var spawn_point:Marker3D = spawn_points[i]
 		var enemy_to_spawn_INSTANCE:Enemy = enemy_to_spawn_SCENE.instantiate()
 		enemy_to_spawn_INSTANCE.global_position = spawn_point.global_position
 		get_tree().current_scene.add_child.call_deferred(enemy_to_spawn_INSTANCE)
 		ret.append(enemy_to_spawn_INSTANCE)
+		# Optional delay between each spawn, measured in seconds and framerate-independent.
+		# SceneTreeTimer created by create_timer() is one-shot and cleans itself up.
+		if spawn_delay_between_enemies > 0.0 and i < spawn_points.size() - 1:
+			await get_tree().create_timer(spawn_delay_between_enemies).timeout
 	return ret
 		
 		
