1ebad7384b1fa19e487217bc4588299ae4f8595f
itsung26 <iat2016@icloud.com>
2026-03-26 11:43:41 -0400

    system

--- scripts/Debug.gd
+++ scripts/Debug.gd
@@ -4,6 +4,7 @@ extends Node
 ## Main debug class for debug-related things. Runs as an autoload singleton. 
 ## (loaded on first startup, never unloaded and can be refrenced from anywhere.)
 
+
 ## Gets a cleaned-up version of the current stack trace and returns it as a concatenated
 ## string.
 func getStackSlim() -> String:
@@ -12,8 +13,16 @@ func getStackSlim() -> String:
 	var r:String = "source: " + lesser_stack["source"] + " function: " + lesser_stack["function"] + " line: " + str(lesser_stack["line"])
 	return r
 
+
 ## Prints the string from of the variant and the stack function and the current line.
 ## Intended to replace default print() behaviour.
 func log(loggable = "") -> void:
 	var stackslim:String = getStackSlim()
 	print(stackslim + "\n" + str(loggable))
+
+
+## Prints the string from of the variant in a standard error line.
+## The stack trace is printed above the error line as a regular print line.
+## Intended to replace default printerr() behaviour.
+func logerr(loggable = "") -> void:
+	pass
--- scripts/time_flow_system.gd
+++ scripts/time_flow_system.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var freeze_timer:Timer
+var _cached_timescale:float
+
 
 func _ready() -> void:
 	freeze_timer = Timer.new()
@@ -8,6 +10,16 @@ func _ready() -> void:
 	freeze_timer.one_shot = true
 	# connect the freeze_timer's signal to this object's listener method
 	freeze_timer.timeout.connect(_on_freeze_timer_timeout)
+	_cached_timescale = Engine.time_scale
+
 
 func _on_freeze_timer_timeout() -> void:
-	pass
+	Engine.time_scale = _cached_timescale
+
+
+func interruptTimeflow(duration:float) -> void:
+	if Engine.time_scale == 0.0:
+		printerr()
+	_cached_timescale = Engine.time_scale
+	Engine.time_scale = 0.0
+	freeze_timer.start(duration)
