f25016cc3035d55be0e70b63f9a6c793e89371bb
itsung26 <iat2016@icloud.com>
2026-07-13 19:38:51 -0400

    weapon

---
+++ scripts/impact_hammer.gd
@@ -0,0 +1,22 @@
+class_name BlastHammer
+extends PlayerWeapon
+
+
+func _onEquipImpl() -> void:
+	pass
+
+
+func _fireImpl() -> void:
+	pass
+
+
+func _specialImpl() -> void:
+	pass
+
+
+func _specialReleaseImpl() -> void:
+	pass
+
+
+func _reloadImpl() -> void:
+	pass
